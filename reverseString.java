8819735ecfeeb323577969e020682f1bbec288ea
beachcoder25 <cornish25@gmail.com>
2019-01-16 21:10:45 -0800

    Reversing a String

---
+++ reverseString.java
@@ -0,0 +1,19 @@
+package reverseString;
+
+public class reverseStringSolution {
+	
+	public static void main(String[] args) {
+		
+		String forwardString = "Apple";
+		String reverseString = "";
+		
+		for(int i= forwardString.length() - 1; i >= 0; i--) {
+			reverseString = reverseString + forwardString.charAt(i);
+			//char x = forwardString.charAt(forwardString.length() - 1 - i);
+			
+		}
+		
+		System.out.println("Original String: " + forwardString + "\nReversed String: " + reverseString);
+		
+	}
+}
